fc09e0085eaf79cbdd3b8f18fe4c64e648875131
arif1101 <arifurrahmanarif223@gmail.com>
2025-06-20 22:25:11 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import {Server} from 'http'
+import app from './app';
+import mongoose from 'mongoose';
+
+let server: Server;
+const PORT = 5000
+async function main() {
+    try{
+        await mongoose.connect('mongodb+srv://bookAdmin:29j6ATFfFo6a2v2M@cluster0.hvsn9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        server = app.listen(PORT, ()=> {
+            console.log(`app is listening on port ${PORT}`)
+        });
+    } catch (error){
+        console.log(error)
+    }
+}
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+// pass : 29j6ATFfFo6a2v2M
+// name : bookAdmin
